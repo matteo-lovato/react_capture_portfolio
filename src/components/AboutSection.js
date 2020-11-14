0058956ef0638ca7ac3d354a36c32609dbc4a21f
matteo_lovato <lovato.teo@gmail.com>
2020-11-14 22:59:09 +0100

    add about section of aboutus page

---
+++ src/components/AboutSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+function AboutSection() {
+  return (
+    <div className="about__section">
+      <div className="about__description">
+        <div className="about__title">
+          <div className="about__title--hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="about__title--hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="about__title--hide">
+            <h2>true.</h2>
+          </div>
+          <p>
+            Contact us for any photography or videography ideas that you have.
+            We have professionals with amazing skills.
+          </p>
+          <button>Contact Us</button>
+        </div>
+      </div>
+      <div className="about__image">
+        <img src={home1} alt="professional photographer with a camera" />
+      </div>
+    </div>
+  );
+}
+
+export default AboutSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+// page components
+import AboutSection from "../components/AboutSection";
+
+function AboutUs() {
+  return (
+    <div className="about__page">
+      <AboutSection />
+    </div>
+  );
+}
+
+export default AboutUs;
