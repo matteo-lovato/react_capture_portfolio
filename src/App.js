e2c53ac2cd42c21ea6cc2b66395c13f44b193bfd
matteo_lovato <lovato.teo@gmail.com>
2020-11-15 22:38:14 +0100

    added the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 // global style
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
