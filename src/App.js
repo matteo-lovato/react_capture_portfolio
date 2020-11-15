93cf7c8e303609dfa23cff2bc584e719342e680d
matteo_lovato <lovato.teo@gmail.com>
2020-11-15 22:55:18 +0100

    Added react router nav links sends you to work and contact pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+// pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+// router
+import { Switch, Route } from "react-router-dom";
 // global style
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
