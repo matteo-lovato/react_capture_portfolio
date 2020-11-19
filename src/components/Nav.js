035c9dfeccfd70a237547511254a3f9c95ee6926
matteo_lovato <lovato.teo@gmail.com>
2020-11-19 15:48:28 +0100

    bug fixes scroll to top on page load navbar sticky

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,6 +32,9 @@ const StyledNav = styled.nav`
   margin: auto;
   align-items: center;
   padding: 1rem 10rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
   background: #282828;
   a {
     color: white;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -17,8 +17,12 @@ import {
   sliderAnimation,
   sliderContainer,
 } from "../animation";
+import { useScroll } from "../components/useScroll";
+import ScrollTop from "../components/ScrollTop";
 
 function OurWork() {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
   return (
     <Work
       variants={pageAnimation}
@@ -43,7 +47,7 @@ function OurWork() {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element} variants={fade} animate={controls} initial="hidden">
         <motion.h2 variants={fade}>The Racer</motion.h2>
         <motion.div variants={lineAnimation} className="line"></motion.div>
         <Link to="/work/the-racer">
@@ -56,7 +60,12 @@ function OurWork() {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie
+        ref={element2}
+        variants={fade}
+        animate={controls2}
+        initial="hidden"
+      >
         <motion.h2 variants={fade}>Good Times</motion.h2>
         <motion.div variants={lineAnimation} className="line"></motion.div>
         <Link to="/work/good-times">
@@ -69,6 +78,7 @@ function OurWork() {
           </Hide>
         </Link>
       </Movie>
+      <ScrollTop />
     </Work>
   );
 }
@@ -82,7 +92,7 @@ const Work = styled(motion.div)`
   }
 `;
 
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
